aa1ecfb4ac6f756847e0562372f864e535c5ed6b
Randerson Lemos <randerson.lemos@gmail.com>
2023-11-23 09:43:57 -0300

    Added more plots

--- main.py
+++ main.py
@@ -10,6 +10,29 @@ matplotlib.use('TkAgg')
 with open('legend.yaml') as fh:
     legend = yaml.safe_load(fh)
 
+class PlotMap:
+    @classmethod
+    def plot(cls, maps):
+        length = len(maps)
+        fig, axs = plt.subplots(length, 4, sharey=True)
+        if length == 1:
+            axs = axs.reshape(1,2)
+
+        for en, mapp in enumerate(maps):
+            axs[en,0].imshow(mapp.layerBackGround())
+            axs[en,0].set_title('BackGround ({})'.format(mapp.name))
+
+            axs[en,1].imshow(mapp.layerForeGround())
+            axs[en,1].set_title('ForeGround ({})'.format(mapp.name))
+
+            axs[en,2].imshow(mapp.layerClssIntere())
+            axs[en,2].set_title('ClassIntere ({})'.format(mapp.name))
+
+            axs[en,3].imshow(mapp.layerAll())
+            axs[en,3].set_title('All ({})'.format(mapp.name))
+ 
+
+
 
 class Map:
     def __init__(self, name, filePath, classInterestId):
@@ -20,28 +43,33 @@ class Map:
 
         self.mat = mat
 
-        self.classInterestId = classInterestId
 
+        self.backGroundId = 1
         self.backGround = (self.mat == 0)*1
 
+        self.foreGroundId = 2
         self.foreGround = (self.backGround == 0)*1
 
+        self.clssIntereId = classInterestId
         self.clssIntere = (mat == classInterestId)*1
 
 
-    def maskAll(self):
-        return self.foreGround + self.clssIntere
+    def layerBackGround(self):
+        return self.backGround[0,:,:]
 
-    def plot(self):
-        fig, axs = plt.subplots(1, 2, sharey=True)
 
-        axs[0].imshow(self.foreGround[0,:,:])
-        axs[0].set_title('ForeGround')
+    def layerForeGround(self):
+        return self.foreGround[0,:,:]
 
-        axs[1].imshow(self.clssIntere[0,:,:] + self.foreGround[0,:,:])
-        axs[1].set_title('ClassOfInterest {}'.format(self.classInterestId))
 
-        fig.suptitle(self.name)
+    def layerClssIntere(self):
+        return self.clssIntere[0,:,:]
+
+
+    def layerAll(self):
+        return ( self.foreGroundId*(1 - self.clssIntere)*self.foreGround \
+                + self.clssIntereId*self.clssIntere )[0,:,:]
+
 
 
 
@@ -62,10 +90,7 @@ if __name__ == '__main__':
 
 
 
-    cam2022.plot()
-    cam2021.plot()
 
-
+    PlotMap.plot([cam2022, cam2021])
     plt.show()
 
-    import IPython; IPython.embed()
